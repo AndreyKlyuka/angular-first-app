f8d93e35b3e4bfdbb3c2744a4ba26dae0879cb01
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-07-20 16:18:40 +0300

    created product card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { IProduct } from './models/product'
+import { products as data } from './data/products'
+// import { ProductService } from './services/product.service'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-first-app';
+export class AppComponent implements OnInit {
+    title = 'app works!'
+
+    products: IProduct[] = data
+
+    // constructor(private productsService: ProductService) {}
+
+    ngOnInit(): void {
+        throw new Error('Method not implemented.')
+    }
 }
