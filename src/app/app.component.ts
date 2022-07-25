b9235812eb66741bfe7413a4c621980703127f7b
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-07-25 22:26:19 +0300

    feat: add modal service to control modal opening and closing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Observable, tap } from 'rxjs'
 import { IProduct } from './models/product'
+import { ModalService } from './services/modal.service'
 import { ProductService } from './services/product.service'
 
 @Component({
@@ -15,7 +16,10 @@ export class AppComponent implements OnInit {
     loading: boolean = false
     term = ''
 
-    constructor(private productsService: ProductService) {}
+    constructor(
+        private productsService: ProductService,
+        public modalService: ModalService
+    ) {}
 
     ngOnInit(): void {
         this.loading = true
