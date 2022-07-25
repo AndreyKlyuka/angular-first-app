2a418aed845964fa34c19d4641afc03611df3556
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-07-25 21:56:19 +0300

    feat: add modal to create new product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,7 +14,7 @@ export class ProductService {
         return this.http
             .get<IProduct[]>('https://fakestoreapi.com/products', {
                 params: new HttpParams({
-                    fromObject: { limit: 20 },
+                    fromObject: { limit: 5 },
                 }),
             })
             .pipe(catchError(this.errorHandler.bind(this)))
